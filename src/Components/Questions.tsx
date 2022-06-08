1ba5feb81b47e5fad8b87b741a7d940cac3a592c
VincentCarlosTrono <trono.vincentcii@gmail.com>
2022-06-09 00:39:55 +0800

    ready to deploy

--- src/Components/Questions.tsx
+++ src/Components/Questions.tsx
@@ -8,7 +8,6 @@ const Questions = () => {
       answer:
         "Plants are easy way to add color energy and transform your space but which planet is for you. choosing the right plant.",
       isOpen: false,
-      duration: "1000",
     },
     {
       itemNumber: 2,
@@ -16,7 +15,6 @@ const Questions = () => {
       answer:
         "Plants are easy way to add color energy and transform your space but which planet is for you. choosing the right plant.",
       isOpen: false,
-      duration: "2000",
     },
     {
       itemNumber: 3,
@@ -24,7 +22,6 @@ const Questions = () => {
       answer:
         "Plants are easy way to add color energy and transform your space but which planet is for you. choosing the right plant.",
       isOpen: false,
-      duration: "3000",
     },
     {
       itemNumber: 4,
@@ -32,7 +29,6 @@ const Questions = () => {
       answer:
         "Plants are easy way to add color energy and transform your space but which planet is for you. choosing the right plant.",
       isOpen: false,
-      duration: "4000",
     },
     {
       itemNumber: 5,
@@ -40,7 +36,6 @@ const Questions = () => {
       answer:
         "Plants are easy way to add color energy and transform your space but which planet is for you. choosing the right plant.",
       isOpen: false,
-      duration: "5000",
     },
     {
       itemNumber: 6,
@@ -48,12 +43,11 @@ const Questions = () => {
       answer:
         "Plants are easy way to add color energy and transform your space but which planet is for you. choosing the right plant.",
       isOpen: false,
-      duration: "6000",
     },
   ]);
 
   const toggleInfo = (info) => {
-    const { question, answer, isOpen, itemNumber, duration } = info;
+    const { question, answer, isOpen, itemNumber } = info;
     setInfo((prevValue) => {
       const filteredList = prevValue.filter(
         (info) => info.itemNumber !== itemNumber
@@ -63,7 +57,6 @@ const Questions = () => {
         answer,
         isOpen: !isOpen,
         itemNumber,
-        duration,
       });
       const newList = filteredList.sort((a, b) => a.itemNumber - b.itemNumber);
 
@@ -71,10 +64,7 @@ const Questions = () => {
     });
   };
   return (
-    <div
-      className="grid-container bg-lightgreen dark:bg-darkmodeLightGreen md:p-20 py-10"
-      data-aos="fade-up"
-    >
+    <div className="grid-container bg-lightgreen dark:bg-darkmodeLightGreen md:p-20 py-10">
       <div className="col-start-2 cols-end-3 ">
         <h1 className="md:text-5xl text-2xl font-semibold md:text-left text-center md:max-w-2xl py-10 dark:text-white">
           Some common questions were often asks
@@ -82,7 +72,7 @@ const Questions = () => {
 
         <div className="grid md:grid-cols-2 gap-5">
           {infos.map((info: any, index) => {
-            const { question, answer, isOpen, duration } = info;
+            const { question, answer, isOpen } = info;
 
             return (
               <div>
@@ -110,7 +100,6 @@ const Questions = () => {
                   <div
                     className="md:px-10  items-start md:gap-10 bg-white dark:bg-darkmodeDarkGreen"
                     data-aos="fade-up"
-                    data-aos-duration={duration}
                     onClick={() => toggleInfo(info)}
                   >
                     <button className="px-8 text-md flex justify-center items-center md:gap-10">
